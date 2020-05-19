0a43d200b74039db5616f31473d27d5b43745de2
永山奈留未 <k016a1001@gmail.com>
2020-05-20 00:26:10 +0900

    Finish 3-8

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '18ad1cfcfd6297d83eb1a8acc87ad3b8d900a2bf0c8a62d4296d90d1ea3efe18bcc9020b7dcf58ed6fb9db56d9433f099988870811b89555d4ade0448cb6ba8f'
