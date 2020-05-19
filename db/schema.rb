471917f5e8702f41b0ecc510297fd0f08dc675cc
永山奈留未 <k016a1001@gmail.com>
2020-05-19 23:39:38 +0900

    Start 3-8

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_18_071405) do
+ActiveRecord::Schema.define(version: 2020_05_19_142130) do
 
   create_table "lists", force: :cascade do |t|
     t.string "title"
     t.string "body"
+    t.string "image_id"
   end
 
 end
